f3761b61370f32046d33a4453a1ef20d1925c21a
vinyzada <vinyescolas@gmail.com>
2025-04-22 09:09:58 -0300

    feat: criada api para acessorio

---
+++ core/models/acessorio.py
@@ -0,0 +1,8 @@
+from pyexpat import model
+from django.db import models
+
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100, verbose_name='Descrição')
+
+    def __str__(self):
+        return f'({self.id}) {self.descricao}'
