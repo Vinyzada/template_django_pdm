e22aa8d0c9756d403e4d6dd0bca6d4c5feddab59
vinyzada <vinyescolas@gmail.com>
2025-04-25 14:55:27 -0300

    feat: Alterado configurações dos campos ano e preco da tabela Veiculo

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Veiculo(models.Model):
-    ano = models.IntegerField()
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    ano = models.IntegerField(null=True, blank=True, default=0)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     cor = models.ForeignKey('Cor', on_delete=models.CASCADE, related_name='veiculos')
     modelo = models.ForeignKey('Modelo', on_delete=models.CASCADE, related_name='veiculos')
     acessorios = models.ManyToManyField('Acessorio', related_name='veiculos')
